4a05ce2ebff4e91e11b20b339a6ebbb71b412e7d
Grahame Thomson <mrgrahamethomson@gmail.com>
2020-03-13 09:30:08 -0400

    Updates

---
+++ precache-manifest.c222b5c85fd1ca9d39ffb1f3ee82f8fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302a7e77d70e9d9ccb73a97e649b4a9c",
+    "url": "/frugal-life/index.html"
+  },
+  {
+    "revision": "3aef5375eca0380a31ff",
+    "url": "/frugal-life/static/css/main.55e69184.chunk.css"
+  },
+  {
+    "revision": "d259c0037a0391491ff8",
+    "url": "/frugal-life/static/js/2.80b5d3e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/frugal-life/static/js/2.80b5d3e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aef5375eca0380a31ff",
+    "url": "/frugal-life/static/js/main.a2eda118.chunk.js"
+  },
+  {
+    "revision": "1f169b3fd8321db94e79",
+    "url": "/frugal-life/static/js/runtime-main.9c990ad3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frugal-life/static/media/logo.5d5d9eef.svg"
+  }
+]);
